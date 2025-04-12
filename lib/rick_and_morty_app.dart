fbf266aca97e5deea7bbfaf2b9275ca72992c522
Varya Kush <barbarakushaeva@gmail.com>
2025-04-12 11:34:56 +0300

    created structure

--- lib/rick_and_morty_app.dart
+++ lib/rick_and_morty_app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:rick_and_morty/router/router.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class RickAndMortyApp extends StatefulWidget {
   const RickAndMortyApp({super.key});
@@ -8,8 +11,16 @@ class RickAndMortyApp extends StatefulWidget {
 }
 
 class _RickAndMortyAppState extends State<RickAndMortyApp> {
+  final _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: 'CryptoCurrencies',
+      routerConfig: _appRouter.config(
+        navigatorObservers: () => [TalkerRouteObserver(GetIt.I<Talker>())],
+      ),
+    );
   }
 }
